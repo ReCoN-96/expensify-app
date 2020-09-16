bdddcaf7cc066b5f7f33a378f774029bf58069ed
vincent <osf@hotmail.fr>
2020-09-16 15:02:21 +0200

    clean default value in app file

--- src/app.js
+++ src/app.js
@@ -12,11 +12,6 @@ import 'react-dates/lib/css/_datepicker.css'
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill', amount: 2500, createdAt: 2004 }));
-store.dispatch(addExpense({ description: 'Gas bill', amount: 1000, createdAt: 2005 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 40000, createdAt: 2004 }));
-store.dispatch(addExpense({ description: 'facture dupond', amount: 2000, createdAt: 2020 }));
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
