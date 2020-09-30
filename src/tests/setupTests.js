b240b4faf2b6147308fe11936f80bba1600e1964
vincent <osf@hotmail.fr>
2020-09-30 15:12:32 +0200

    Setup test database

--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -1,6 +1,8 @@
 import Enzyme from 'enzyme';
 import adapter from 'enzyme-adapter-react-16';
 
+require('dotenv').config({ path: '.env.test' });
+
 Enzyme.configure({
   adapter: new adapter()
-})
+});
